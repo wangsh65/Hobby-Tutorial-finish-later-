8285d0b3ee96c8313b7775dbf15125232c8a4774
wangsh65 <wangsh@ssfs.org>
2024-05-03 14:12:21 -0400

    new

--- Hobby Tutorial./ViewController.swift
+++ Hobby Tutorial./ViewController.swift
@@ -5,12 +5,34 @@
 //  Created by Apple on 2024/2/29.
 //
 
+
+import SwiftUI
 import UIKit
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController {
+    var audio1 : String?
+    
+    @IBAction func song1(_ sender: UIButton) {
+        audio1 = " afterlike"
+        performSegue(withIdentifier: "segueToAudioPlayer", sender: nil)
+        
+        
+    }
+    
+    @IBAction func song2(_ sender: UIButton) {
+    }
+    
+    @IBAction func song3(_ sender: UIButton) {
+    }
+    @IBAction func song4(_ sender: UIButton) {
+    }
+    @IBAction func song5(_ sender: UIButton) {
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        
         // Do any additional setup after loading the view.
     }
 
